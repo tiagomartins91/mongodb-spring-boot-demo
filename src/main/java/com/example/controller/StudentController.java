023ee568325f07bbd7f37310aa7a9cf2b4b6ada1
tiago.jesus <tiago.jesus@unilabs.com>
2021-05-10 16:23:29 +0100

    Add findById API

--- src/main/java/com/example/controller/StudentController.java
+++ src/main/java/com/example/controller/StudentController.java
@@ -2,13 +2,10 @@ package com.example.controller;
 
 import com.example.entity.Student;
 import com.example.service.StudentService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/student")
+@RequestMapping("/api/students")
 public class StudentController {
 
     private final StudentService studentService;
@@ -17,9 +14,14 @@ public class StudentController {
         this.studentService = studentService;
     }
 
-    @PostMapping("/create")
+    @PostMapping
     public Student createStudent(@RequestBody Student student) {
         return studentService.createStudent(student);
+    }
+
+    @GetMapping(value = "/{id}")
+    public Student findById(@PathVariable("id") String id) {
+        return studentService.findById(id);
     }
 
 }
--- src/main/java/com/example/service/StudentService.java
+++ src/main/java/com/example/service/StudentService.java
@@ -5,6 +5,8 @@ import com.example.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class StudentService {
 
@@ -13,5 +15,12 @@ public class StudentService {
 
     public Student createStudent(Student student) {
         return studentRepository.save(student);
+    }
+
+    public Student findById(String id) {
+        return Optional.of(studentRepository.findById(id))
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .orElse(null);
     }
 }
