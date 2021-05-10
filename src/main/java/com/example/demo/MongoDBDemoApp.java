93bf46285fa525facaf0f9b4fb650e1057a8fe16
tiago.jesus <tiago.jesus@unilabs.com>
2021-05-10 15:01:55 +0100

    Create Spring Boot Application

---
+++ src/main/java/com/example/demo/MongoDBDemoApp.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDBDemoApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoDBDemoApp.class, args);
+	}
+
+}
